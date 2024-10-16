2ef77c7ced1c483a9cf6320e66e1c4f433e79a75
arslan <saifiarslaan01@gmail.com>
2024-10-15 23:07:51 -0700

    today class 16oct

---
+++ demogitpush/AccessModifier.js
@@ -0,0 +1,22 @@
+
+class a{
+    constructor(name,age,education){
+        this.naam=name
+        this.umar=age
+        this.padhai=education
+    }
+}
+
+class b extends a{
+    constructor(name,age){
+        super(naam,age)
+        this.naam=name
+        this.umar=age
+    }
+}
+
+
+
+let rtb= new b("arslan",19)
+
+console.log(rtb);
